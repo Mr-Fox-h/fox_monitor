b7e7abceffe16c5cc88bf974cf31372de7a85954
Mr-Fox-h <mr.fox@iran.ir>
2024-12-15 13:04:47 +0330

    Fixed: CPU usage calculation.

--- include/cpu_monitor.h
+++ include/cpu_monitor.h
@@ -5,7 +5,7 @@
 
 #define MAX_CPUS 128
 
-void cpu_monitoring(char cpus[][256], char *intr) {
+void cpu_monitoring(char cpus[][256]) {
   char buffer[256];
   int cpu_count = 0;
 
@@ -19,10 +19,42 @@ void cpu_monitoring(char cpus[][256], char *intr) {
       strncpy(cpus[cpu_count], buffer, 256);
       cpu_count++;
     }
-    if (strncmp(buffer, "intr ", 5) == 0) {
-      strncpy(intr, buffer, 256);
-    }
   }
 
   fclose(file);
+}
+void parse_cpu_stats(const char *cpu_line, unsigned long long *user,
+                     unsigned long long *nice, unsigned long long *system,
+                     unsigned long long *idle, unsigned long long *iowait,
+                     unsigned long long *irq, unsigned long long *softirq,
+                     unsigned long long *steal, unsigned long long *guest,
+                     unsigned long long *guest_nice) {
+  sscanf(cpu_line, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+         user, nice, system, idle, iowait, irq, softirq, steal, guest,
+         guest_nice);
+}
+
+double calculate_cpu_usage(unsigned long long user1, unsigned long long nice1,
+                           unsigned long long system1, unsigned long long idle1,
+                           unsigned long long iowait1, unsigned long long irq1,
+                           unsigned long long softirq1,
+                           unsigned long long steal1, unsigned long long guest1,
+                           unsigned long long guest_nice1,
+                           unsigned long long user2, unsigned long long nice2,
+                           unsigned long long system2, unsigned long long idle2,
+                           unsigned long long iowait2, unsigned long long irq2,
+                           unsigned long long softirq2,
+                           unsigned long long steal2, unsigned long long guest2,
+                           unsigned long long guest_nice2) {
+  unsigned long long total1 = user1 + nice1 + system1 + idle1 + iowait1 + irq1 +
+                              softirq1 + steal1 + guest1 + guest_nice1;
+  unsigned long long total2 = user2 + nice2 + system2 + idle2 + iowait2 + irq2 +
+                              softirq2 + steal2 + guest2 + guest_nice2;
+  unsigned long long idle_diff = idle2 - idle1;
+  unsigned long long total_diff = total2 - total1;
+
+  if (total_diff == 0)
+    return 0.0;
+
+  return 100.0 * (1.0 - (double)idle_diff / total_diff);
 }
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "include/cpu_monitor.h"
+#include "include/cpu_progress_bar.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,17 +7,40 @@
 
 int main(int argc, char *argv[]) {
   // CPU Monitoring
-  char cpus[MAX_CPUS][256];
-  char intr[256];
+  char cpus[MAX_CPUS][256] = {{0}};
+  unsigned long long user1[MAX_CPUS], nice1[MAX_CPUS], system1[MAX_CPUS],
+      idle1[MAX_CPUS], iowait1[MAX_CPUS], irq1[MAX_CPUS], softirq1[MAX_CPUS],
+      steal1[MAX_CPUS], guest1[MAX_CPUS], guest_nice1[MAX_CPUS];
+  unsigned long long user2[MAX_CPUS], nice2[MAX_CPUS], system2[MAX_CPUS],
+      idle2[MAX_CPUS], iowait2[MAX_CPUS], irq2[MAX_CPUS], softirq2[MAX_CPUS],
+      steal2[MAX_CPUS], guest2[MAX_CPUS], guest_nice2[MAX_CPUS];
 
   while (true) {
-    cpu_monitoring(cpus, intr);
+    cpu_monitoring(cpus);
     for (int i = 0; i < MAX_CPUS && cpus[i][0] != '\0'; i++) {
-      printf("%s", cpus[i]);
+      parse_cpu_stats(cpus[i], &user1[i], &nice1[i], &system1[i], &idle1[i],
+                      &iowait1[i], &irq1[i], &softirq1[i], &steal1[i],
+                      &guest1[i], &guest_nice1[i]);
     }
-    printf("Intr: %s\n", intr);
-    usleep(100000);
+
+    sleep(1);
+    cpu_monitoring(cpus);
+    for (int i = 0; i < MAX_CPUS && cpus[i][0] != '\0'; i++) {
+      parse_cpu_stats(cpus[i], &user2[i], &nice2[i], &system2[i], &idle2[i],
+                      &iowait2[i], &irq2[i], &softirq2[i], &steal2[i],
+                      &guest2[i], &guest_nice2[i]);
+    }
+
     system("clear");
+    for (int i = 0; i < MAX_CPUS && cpus[i][0] != '\0'; i++) {
+      double usage = calculate_cpu_usage(
+          user1[i], nice1[i], system1[i], idle1[i], iowait1[i], irq1[i],
+          softirq1[i], steal1[i], guest1[i], guest_nice1[i], user2[i], nice2[i],
+          system2[i], idle2[i], iowait2[i], irq2[i], softirq2[i], steal2[i],
+          guest2[i], guest_nice2[i]);
+      printf("CPU%d:\t", i);
+      print_progress_bar(usage);
+    }
   }
   return EXIT_SUCCESS;
 }

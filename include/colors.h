bc07a5615117245c91afb7898c18cc448cb90d33
Mr-Fox-h <mr.fox@iran.ir>
2024-12-18 20:50:31 +0330

    Add: Color File

---
+++ include/colors.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void red() { printf("\033[1;31m"); }
+void green() { printf("\033[0;32m"); }
+void yellow() { printf("\033[1;33m"); }
+void reset_color() { printf("\033[0m"); }
